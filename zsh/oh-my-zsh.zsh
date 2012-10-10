2fd905796c6e80993a0ee4c0231a031334619058
Matt Button <matthew@sigswitch.com>
2012-10-10 14:22:57 +0100

    Updating submodules

--- zsh/oh-my-zsh.zsh
+++ zsh/oh-my-zsh.zsh
@@ -3,7 +3,7 @@
 ZSH_THEME="fino"
 DISABLE_AUTO_UPDATE="true"
 
-plugins=(rails3 autojump terminator thor github heroku command-not-found rvm zsh-syntax-highlighting rbenv bundler)
+plugins=(rails3 terminator thor github heroku command-not-found rvm zsh-syntax-highlighting rbenv bundler)
 
 source $HOME/.zshenv
 
