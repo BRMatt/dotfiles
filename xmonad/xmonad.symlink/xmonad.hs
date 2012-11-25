a996402b51f506e3f5c7cc8420a913d9b3a5fbf5
Matt Button <matthew@sigswitch.com>
2012-11-25 14:03:48 +0000

    Got a basic xmobar setup working

--- xmonad/xmonad.symlink/xmonad.hs
+++ xmonad/xmonad.symlink/xmonad.hs
@@ -5,8 +5,28 @@ import XMonad.Prompt.RunOrRaise
 import XMonad.Util.EZConfig(additionalKeys)
 import qualified XMonad.StackSet as W
 import XMonad.Hooks.ManageHelpers
+import XMonad
+import XMonad.Hooks.DynamicLog
 import XMonad.Layout.NoBorders
 import XMonad.Layout.Fullscreen
+import XMonad.Util.Run(spawnPipe)
+
+------------------------------------------------------------------------
+-- Terminal
+-- The preferred terminal program, which is used in a binding below and by
+-- certain contrib modules.
+--
+myTerminal = "/usr/bin/gnome-terminal"
+
+------------------------------------------------------------------------
+-- Status bar configuration
+------------------------------------------------------------------------
+myBar = "xmobar"
+
+toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
+
+
+myWorkspaces = ["1:web", "2:dev", "3:music", "4:comm", "5", "6", "7", "8", "9", "0", "-", "="]
 
 
 ------------------------------------------------------------------------
@@ -22,13 +42,13 @@ myManageHook = composeAll
   , isFullscreen --> (doF W.focusDown <+> doFullFloat)
   ]
 
-main = xmonad $ gnomeConfig
-  { manageHook = myManageHook
-  , workspaces = ["1:web", "2:dev", "3:music", "4:comm", "5", "6", "7", "8", "9", "0", "-", "="]
+main = xmonad =<< xmobar myConfig
+
+
+myConfig = gnomeConfig { manageHook = myManageHook
+  , workspaces = myWorkspaces
   }
   `additionalKeys`
   [ ((mod1Mask, xK_F2), runOrRaisePrompt defaultXPConfig)
   ]
-
-
 
