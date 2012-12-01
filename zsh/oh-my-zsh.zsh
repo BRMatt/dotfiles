d54ccc130bbc229e8184d0d8aaa9696d5327d807
Matt Button <matthew@sigswitch.com>
2012-12-01 23:44:13 +0000

    Updating OMZ and changing zsh plugins

--- zsh/oh-my-zsh.zsh
+++ zsh/oh-my-zsh.zsh
@@ -3,7 +3,7 @@
 ZSH_THEME="fino"
 DISABLE_AUTO_UPDATE="true"
 
-plugins=(rails3 terminator thor github command-not-found rvm zsh-syntax-highlighting rbenv bundler)
+plugins=(rails3 terminator thor gitfast github command-not-found zsh-syntax-highlighting rbenv bundler history-substring-search)
 
 source $HOME/.zshenv
 
